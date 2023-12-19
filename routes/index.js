133fdd7ea575894320604452fe683a591fbfc4ad
Walter Fritsche <wfritsche95@protonmail.com>
2023-12-19 14:29:23 -0600

    formatted and made a few changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+const indexController = require("../controllers/indexController");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "VIP Message Board" });
 });
+
+// GET signup page
+router.get("/signup", indexController.signup_get);
+
+// POST signup
+router.post("/signup", indexController.signup_post);
 
 module.exports = router;
