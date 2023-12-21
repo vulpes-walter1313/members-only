1607a276bce2678b4e4744c3ac6c69e73f2f0958
Walter Fritsche <wfritsche95@protonmail.com>
2023-12-20 18:30:14 -0600

    added routes for root routes

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,32 @@ router.get("/signup", indexController.signup_get);
 // POST signup
 router.post("/signup", indexController.signup_post);
 
+router.get("/login", (req, res) => {
+  res.send("'/login' GET not implemented");
+});
+
+router.post("/login", (req, res) => {
+  res.send("'/login' POST not implemented");
+});
+
+router.get("/membership", (req, res) => {
+  res.send("/membership GET route not implemented");
+});
+
+router.post("/membership", (req, res) => {
+  res.send("/membership POST route not implemented");
+});
+
+router.get("/welcome-new-member", (req, res) => {
+  res.send("/welcome-new-member GET route not implemented");
+});
+
+router.get("/become-an-admin", (req, res) => {
+  res.send("/become-an-admin GET route not implemented");
+});
+
+router.post("/become-an-admin", (req, res) => {
+  res.send("/become-an-admin POST route not implemented");
+});
+
 module.exports = router;
